5bf7890a0af3bdcab47b91b611e77c2ae85eb72d
sbrandt409 <sbrandt409@gmail.com>
2014-05-30 16:04:10 -0500

    Initial Check-in

---
+++ AspNetGroupBasedPermissions/Models/ApplicationUser.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace AspNetGroupBasedPermissions.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public ApplicationUser()
+            : base()
+        {
+            this.Groups = new HashSet<ApplicationUserGroup>();
+        }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        public virtual ICollection<ApplicationUserGroup> Groups { get; set; }
+
+    }
+}
---
+++ AspNetGroupBasedPermissions/Security/AccessDeniedAuthorizeAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace AspNetGroupBasedPermissions.Security
+{
+    public class AccessDeniedAuthorizeAttribute : AuthorizeAttribute
+    {
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            base.OnAuthorization(filterContext);
+
+            if (filterContext.Result is HttpUnauthorizedResult)
+            {
+                //filterContext.Result = new RedirectResult("~/AcessDenied.aspx");
+                filterContext.Result = new RedirectToRouteResult(new
+                     RouteValueDictionary(new { controller = "Error", action = "AccessDenied" }));
+            }
+        }
+    }
+}
