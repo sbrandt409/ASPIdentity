c0e84dad031cc542a6ce594916a6ad6e03602ad2
sbrandt409 <sbrandt409@gmail.com>
2014-06-03 16:00:56 -0500

    Paging, Sorting, Filtering with JQuery DataTables

--- AspNetGroupBasedPermissions/App_Start/BundleConfig.cs
+++ AspNetGroupBasedPermissions/App_Start/BundleConfig.cs
@@ -8,16 +8,16 @@ namespace AspNetGroupBasedPermissions
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //            "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            //            "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //            "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
@@ -26,6 +26,16 @@ namespace AspNetGroupBasedPermissions
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/table").Include(
+               "~/Scripts/DataTables-1.9.4/media/js/jquery.dataTables.js"));
+
+            //bundles.Add(new ScriptBundle("~/bundles/jQueryPlugins").Include(
+            //    "~/Scripts/DataTables-1.9.4/media/js/jquery.dataTables.min.js"));
+
+            //bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+            //            "~/Content/DataTables-1.9.4/media/css/jquery.dataTables.css"));
+
         }
     }
 }
